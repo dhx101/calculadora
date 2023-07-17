4a2bf7cda428d1e51719fe927fc2bcd28667a469
dhx101 <138037766+dhx101@users.noreply.github.com>
2023-07-17 23:17:46 +0200

    Added the basic funtionality of a calculator Added Add, subtract, multiply and divide

---
+++ main.js
@@ -0,0 +1,12 @@
+let add = (actual, value) => actual += value;
+
+let subtract = (actual, value) => actual -= value;
+
+let multiply = (actual, value) => actual * value;
+
+let divide =  (actual, value) => actual / value
+
+console.log(add(1,2))
+console.log(subtract(5,3))
+console.log(multiply(5,3))
+console.log(divide(4,2))
