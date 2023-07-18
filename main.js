d4cc98b38a086e891932ce6c4dcd09e3d1de7579
dhx101 <138037766+dhx101@users.noreply.github.com>
2023-07-18 22:55:51 +0200

    Added basic number funtionality now each number will be added to the end of results

--- main.js
+++ main.js
@@ -6,6 +6,29 @@ let multiply = (actual, value) => actual * value;
 
 let divide =  (actual, value) => actual / value
 
+function changeDisplay(value) {
+    let results = document.querySelector('#resultados');
+    let display = document.querySelector('.display');
+    let newDisplay = document.createElement('p')
+    newDisplay.classList.add('display')
+    let actual = display.textContent
+    if( actual == 0) {
+        newDisplay.textContent = value
+        results.removeChild(results.lastChild)
+        results.appendChild(newDisplay)
+} else {
+        newDisplay.textContent = actual + value
+        results.removeChild(results.lastChild)
+        results.appendChild(newDisplay)
+}}
+    
+function getBtn() {
+    let btn = document.querySelectorAll(button)
+    
+}
+
+
+changeDisplay(3)
 console.log(add(1,2))
 console.log(subtract(5,3))
 console.log(multiply(5,3))
